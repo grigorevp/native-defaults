659e303aa0b7f5e2621bc5a87e692a207fc18f8b
Petr-Grigorev <grigoryev.pete@gmail.com>
2021-02-23 11:33:19 +0300

    Added publish.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
 plugins {
-    kotlin("multiplatform") version "1.4.30"
     id("com.android.library")
+    kotlin("multiplatform") version "1.4.30"
     id("maven-publish")
     signing
 }
-
-apply("publish.gradle.kts")
 
 repositories {
     google()
@@ -14,7 +12,9 @@ repositories {
 }
 
 kotlin {
-    android()
+    android {
+        publishLibraryVariants("release")
+    }
     iosX64("ios") {
         binaries {
             framework {
@@ -32,13 +32,13 @@ kotlin {
         }
         val androidMain by getting {
             dependencies {
-                implementation("com.google.android.material:material:1.2.1")
+                implementation("com.google.android.material:material:1.3.0")
             }
         }
         val androidTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
-                implementation("junit:junit:4.13")
+                implementation("junit:junit:4.13.2")
             }
         }
         val iosMain by getting
@@ -49,8 +49,12 @@ kotlin {
 android {
     compileSdkVersion(29)
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].java.srcDirs("src/androidMain/kotlin")
+    sourceSets["main"].resources.srcDirs("src/androidMain/resources")
     defaultConfig {
         minSdkVersion(24)
         targetSdkVersion(29)
     }
 }
+
+apply(from = "publish.gradle.kts")
